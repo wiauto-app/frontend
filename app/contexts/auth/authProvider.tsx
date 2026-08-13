ff4bb4f6fbbf9fd89c7f1cbaaa449aa673d3e6d9
Idahir2406 <ipincay5178@utm.edu.ec>
2026-08-13 00:39:46 -0500

    refactor: enhance AuthProvider with user logout functionality and routing
    - Integrated useRouter for improved navigation after user logout.
    - Updated logout function to refresh the router state post-logout.
    - Cleaned up code for better readability and maintainability.

--- app/contexts/auth/authProvider.tsx
+++ app/contexts/auth/authProvider.tsx
@@ -4,11 +4,12 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 
 import { authService, type MeResponseDto } from "@/services/authService";
 import { AuthContext } from "./authContext";
+import { useRouter } from "next/navigation";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<MeResponseDto | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(false);
-
+  const router = useRouter();
   const refreshUser = useCallback(async () => {
     try {
       setIsLoading(true);
@@ -32,7 +33,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const logout = useCallback(async () => {
     await authService.logout();
     setUser(undefined);
-  }, []);
+    router.refresh();
+  }, [router]);
 
   const value = useMemo(
     () => ({
